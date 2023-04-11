408f4f6aa71555f613c31acff761b83685abcd6d
zestynorsegod <zestynorsegod@gmail.com>
2023-04-11 03:09:25 -0600

    Work on app.

--- WeatherApp/weather-app/src/main/java/com/prigby/MacroPane.java
+++ WeatherApp/weather-app/src/main/java/com/prigby/MacroPane.java
@@ -1,5 +1,7 @@
 package com.prigby;
 
+import com.google.gson.annotations.JsonAdapter;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -13,16 +15,26 @@ import javafx.scene.paint.Color;
 public class MacroPane extends VBox {
 
     public MacroPane() {
+        this.setAlignment(Pos.CENTER);
+
         setHeight(BASELINE_OFFSET_SAME_AS_HEIGHT);
 
         // set bg color, border rounding(??), and border insets(??)
         setBackground(new Background((
                         new BackgroundFill(Color.ANTIQUEWHITE, CornerRadii.EMPTY, Insets.EMPTY))));
 
+
+        // fetch API data
+        
+
+
+
+        //XXX TOP OF VBOX
         Label temperature = new Label("42");
         temperature.setAlignment(Pos.CENTER);
+        temperature.setStyle(String.format("-fx-font-size: %d", 42));
 
-        // bottom horizontal elements
+        //XXX BOTTOM OF VBOX
         HBox hBox = new HBox();
         
 
