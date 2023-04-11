bf90e6c0b3d32c9d52a7d981e8991975239b5478
zestynorsegod <zestynorsegod@gmail.com>
2023-04-10 20:25:40 -0600

    fixing things.

--- WeatherApp/src/main/java/com/prigby/personal/App.java
+++ WeatherApp/src/main/java/com/prigby/personal/App.java
@@ -1,21 +1,26 @@
 package com.prigby.personal;
 
+import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class App {
+public class App extends Application{
     public static void main( String[] args ) {
-        Stage stage = new Stage();
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane);
+        launch(args);    
+    }
 
-        pane.setPrefSize(200, 200);
-        pane.getChildren();
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+        VBox vbox = new VBox();
+        vbox.setPrefSize(200, 200);
+
+        Scene scene = new Scene(vbox, 400, 400);
         
-        
-        stage.setScene(scene);
+
         stage.setTitle("Weather App");
+        stage.setScene(scene);
         stage.show();
     }
 }
