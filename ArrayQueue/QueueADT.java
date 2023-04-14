d4a4e6ef0c84e8fc9f7f672cc342e250f80262a5
zestynorsegod <zestynorsegod@gmail.com>
2023-04-13 22:21:07 -0600

    Removed ArrayQueue folder for relevancy issues.

--- ArrayQueue/QueueADT.java
+++
@@ -1,9 +0,0 @@
-public interface QueueADT<E> {
-
-    public void enqueue(E element);
-    public E dequeue();
-    public E first();
-    public boolean isEmpty();
-    public int size();
-    public String toString();
-}
